6a5b7031013c6d0dd19c7cf7d4bf79f96df8b7b0
James Cox <j-cox03@hotmail.com>
2019-03-04 21:09:27 +0000

    changed how describeConsumerGroup awaits its function calls in the hopes of speeding things up

--- _server-common/kafka/describe-consumer-group/describe-consumer-group.js
+++ _server-common/kafka/describe-consumer-group/describe-consumer-group.js
@@ -2,9 +2,12 @@ const consumerGroupInformation = require("./consumer-group-information");
 const consumerGroupOffsets = require("./consumer-group-offsets");
 
 const describeConsumerGroup = async (topicName, consumerGroupName) => {
+  const information = consumerGroupInformation(consumerGroupName);
+  const offsets = consumerGroupOffsets(topicName, consumerGroupName);
+
   return {
-    ...(await consumerGroupInformation(consumerGroupName)),
-    offsets: await consumerGroupOffsets(topicName, consumerGroupInformation)
+    ...(await information),
+    offsets: await offsets
   };
 };
 
