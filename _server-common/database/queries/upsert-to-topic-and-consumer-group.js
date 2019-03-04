5bc6d9b9e290118198b3e6379e8cd2371e756982
James Cox <j-cox03@hotmail.com>
2019-03-04 16:59:15 +0000

    Stopped using prepare query due to issues with testing

--- _server-common/database/queries/upsert-to-topic-and-consumer-group.js
+++ _server-common/database/queries/upsert-to-topic-and-consumer-group.js
@@ -1,10 +1,15 @@
-const { prepareQuery } = require("../sqlite-connections");
+const { query } = require("../sqlite-connections");
 
-const upsertToTopicAndConsumerGroup = prepareQuery(`
+const upsertToTopicAndConsumerGroup = async (...values) => {
+  return await query(
+    `
   INSERT OR REPLACE INTO topicsAndConsumerGroups
   (topicName, consumerGroupName, lastActive)
   VALUES
   (?, ?, ?)
-`);
+`,
+    values
+  );
+};
 
 module.exports = upsertToTopicAndConsumerGroup;
--- _server-common/database/sqlite-connections.js
+++ _server-common/database/sqlite-connections.js
@@ -10,19 +10,10 @@ const afterQuery = (resolve, reject) => (error, response) => {
   error ? reject(error) : resolve(response);
 };
 
-const query = async query => {
+const query = async (query, values) => {
   return new Promise((resolve, reject) => {
-    db.all(query, afterQuery(resolve, reject));
+    db.all(query, values, afterQuery(resolve, reject));
   });
 };
 
-const prepareQuery = query => {
-  const preparedQuery = db.prepare(query);
-  return async (...values) => {
-    return new Promise((resolve, reject) => {
-      preparedQuery.all(...values, afterQuery(resolve, reject));
-    });
-  };
-};
-
-module.exports = { query, prepareQuery };
+module.exports = { query };
