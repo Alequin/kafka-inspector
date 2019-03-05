829c98c9b4c27122b2d7f8485f6eb88e241a7bf8
James Cox <j-cox03@hotmail.com>
2019-03-05 23:39:11 +0000

    added built project to tracked files

---
+++ build/precache-manifest.6d5f9de07f605b71c6a6bde86ca5e776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "75631a70fb0913b458d5",
+    "url": "/static/js/main.75631a70.chunk.js"
+  },
+  {
+    "revision": "53231898c9517dfce056",
+    "url": "/static/js/2.53231898.chunk.js"
+  },
+  {
+    "revision": "75631a70fb0913b458d5",
+    "url": "/static/css/main.464377fd.chunk.css"
+  },
+  {
+    "revision": "1cf289c925c633273eec891275a2003d",
+    "url": "/index.html"
+  }
+];
