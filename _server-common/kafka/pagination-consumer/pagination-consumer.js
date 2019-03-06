302757d37302443297f74253ddef2543fd9c54fe
James Cox <j-cox03@hotmail.com>
2019-03-06 21:17:07 +0000

    Changed pagination consumer to handle max offsets greater than the latest offset

--- _server-common/kafka/pagination-consumer/pagination-consumer.js
+++ _server-common/kafka/pagination-consumer/pagination-consumer.js
@@ -1,7 +1,8 @@
 const singleConsumer = require("./single-consumer");
+const fetchLatestOffset = require("./../describe-consumer-group/fetch-latest-offsets");
 
 const paginationConsumer = async options => {
-  const consumer = consumerIterator(options);
+  const consumer = await consumerIterator(options);
   const allMessages = {};
   for await (const { partition, messages } of consumer()) {
     allMessages[partition] = messages;
@@ -9,13 +10,23 @@ const paginationConsumer = async options => {
   return allMessages;
 };
 
-const consumerIterator = ({ topic, partitions, offsetRange }) => {
+const consumerIterator = async ({
+  topicName,
+  partitions,
+  offsetRange: { min, max }
+}) => {
+  const latestOffsets = await fetchLatestOffset(topicName);
   return async function*() {
     const orderedPartitions = partitions.sort();
     for (let partition of orderedPartitions) {
+      const maxOffset = Math.min(max, latestOffsets[partition]);
       yield {
         partition,
-        messages: await singleConsumer({ topic, partition, offsetRange })
+        messages: await singleConsumer({
+          topicName,
+          partition,
+          offsetRange: { min, max: maxOffset }
+        })
       };
     }
   };
--- _server-common/kafka/pagination-consumer/single-consumer.js
+++ _server-common/kafka/pagination-consumer/single-consumer.js
@@ -7,15 +7,8 @@ const CONSUMER_OPTIONS = {
   fromOffset: true
 };
 
-const singleConsumer = async ({ topic, partition, offsetRange }) => {
+const singleConsumer = async ({ topicName, partition, offsetRange }) => {
   const minOffset = Math.max(offsetRange.min, 0);
-
-  if (minOffset > offsetRange.max)
-    throw new Error(
-      `Min offset (${minOffset}) cannot be greater than the max offset((${
-        offsetRange.max
-      }))`
-    );
 
   const {
     kafkaNode: { client }
@@ -23,7 +16,7 @@ const singleConsumer = async ({ topic, partition, offsetRange }) => {
 
   const consumer = new kafka.Consumer(
     client,
-    [{ topic, partition, offset: offsetRange.min }],
+    [{ topic: topicName, partition, offset: minOffset }],
     CONSUMER_OPTIONS
   );
 
