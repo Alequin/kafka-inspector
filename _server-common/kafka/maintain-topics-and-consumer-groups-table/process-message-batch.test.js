67950e9a3cbc7db4b9ac8f6bd97b7190ac6e390b
James Cox <j-cox03@hotmail.com>
2019-03-09 20:46:01 +0000

    Fixed bad mock data for new versions in processMessageBatch

--- _server-common/kafka/maintain-topics-and-consumer-groups-table/process-message-batch.test.js
+++ _server-common/kafka/maintain-topics-and-consumer-groups-table/process-message-batch.test.js
@@ -1,6 +1,6 @@
 const { cloneDeep } = require("lodash");
-const mockListTopics = require("mock-test-data/kafka-node/mock-list-topics");
-const mockListGroups = require("mock-test-data/kafka-node/mock-list-groups");
+const mockTopics = require("mock-test-data/data/mock-topics");
+const mockConsumerGroups = require("mock-test-data/data/mock-consumer-groups");
 const BAD_CHARACTER = String.fromCharCode(1);
 const processMessageBatch = require("./process-message-batch");
 
@@ -11,8 +11,8 @@ describe("processMessageBatch", () => {
   // When changed to a string it is in the form: "<groupName><topicName>"
   const mockMessage = {
     key: Buffer.from(
-      ` ${BAD_CHARACTER}${mockListGroups.consumerGroup1}${BAD_CHARACTER}${
-        mockListTopics.topic1
+      ` ${BAD_CHARACTER}${mockConsumerGroups.consumerGroup1}${BAD_CHARACTER}${
+        mockTopics.topic1
       }${BAD_CHARACTER} `
     ),
     timestamp: timeNow
@@ -25,9 +25,9 @@ describe("processMessageBatch", () => {
 
     const expected = [
       {
-        topicName: mockListTopics.topic1,
+        topicName: mockTopics.topic1,
         consumerGroup: {
-          name: mockListGroups.consumerGroup1,
+          name: mockConsumerGroups.consumerGroup1,
           lastActive: timeNow
         }
       }
@@ -41,7 +41,7 @@ describe("processMessageBatch", () => {
       messages: [
         {
           key: Buffer.from(
-            `${BAD_CHARACTER}${mockListTopics.topic1}${BAD_CHARACTER} `
+            `${BAD_CHARACTER}${mockTopics.topic1}${BAD_CHARACTER} `
           ),
           timestamp: timeNow
         }
@@ -60,9 +60,9 @@ describe("processMessageBatch", () => {
 
     const expected = [
       {
-        topicName: mockListTopics.topic1,
+        topicName: mockTopics.topic1,
         consumerGroup: {
-          name: mockListGroups.consumerGroup1,
+          name: mockConsumerGroups.consumerGroup1,
           lastActive: timeNow
         }
       }
@@ -82,9 +82,9 @@ describe("processMessageBatch", () => {
 
     const expected = [
       {
-        topicName: mockListTopics.topic1,
+        topicName: mockTopics.topic1,
         consumerGroup: {
-          name: mockListGroups.consumerGroup1,
+          name: mockConsumerGroups.consumerGroup1,
           lastActive: newTime
         }
       }
