346348fb09f42a14f124119904795de7f4e7ac5d
James Cox <j-cox03@hotmail.com>
2019-03-09 20:40:46 +0000

    Used more generic mock for topicAndConsumerGroupDetailsFromMessage

--- _server-common/kafka/maintain-topics-and-consumer-groups-table/topic-and-consumer-group-details-from-message.test.js
+++ _server-common/kafka/maintain-topics-and-consumer-groups-table/topic-and-consumer-group-details-from-message.test.js
@@ -1,22 +1,13 @@
 jest.mock("../access-global-kafka-connections");
-const accessGlobalKafkaConnections = require("../access-global-kafka-connections");
-accessGlobalKafkaConnections.mockReturnValue({
-  kafkaNode: {
-    admin: {
-      listTopics: callback => {
-        const error = false;
-        callback(error, mockListTopics.response);
-      },
-      listGroups: callback => {
-        const error = false;
-        callback(error, mockListGroups.response);
-      }
-    }
-  }
-});
-
+const mockTopics = require("mock-test-data/data/mock-topics");
 const mockListTopics = require("mock-test-data/kafka-node/mock-list-topics");
-const mockListGroups = require("mock-test-data/kafka-node/mock-list-groups");
+const mockConsumerGroups = require("mock-test-data/data/mock-consumer-groups");
+const mockAccessGlobalKafkaConnectionsImp = require("mock-test-data/mock-access-global-kafka-connections");
+const accessGlobalKafkaConnections = require("../access-global-kafka-connections");
+accessGlobalKafkaConnections.mockReturnValue(
+  mockAccessGlobalKafkaConnectionsImp()
+);
+
 const topicAndConsumerGroupDetailsFromMessage = require("./topic-and-consumer-group-details-from-message");
 
 const BAD_CHARACTER = String.fromCharCode(1);
@@ -29,17 +20,17 @@ describe("topicAndConsumerGroupDetailsFromMessage", () => {
     // When changed to a string it is in the form: "<groupName><topicName>"
     const mockMessage = {
       key: Buffer.from(
-        ` ${BAD_CHARACTER}${mockListGroups.consumerGroup1}${BAD_CHARACTER}${
-          mockListTopics.topic1
+        ` ${BAD_CHARACTER}${mockConsumerGroups.consumerGroup1}${BAD_CHARACTER}${
+          mockTopics.topic1
         }${BAD_CHARACTER} `
       ),
       timestamp: timeNow
     };
 
     const expected = {
-      topicName: mockListTopics.topic1,
+      topicName: mockTopics.topic1,
       consumerGroup: {
-        name: mockListGroups.consumerGroup1,
+        name: mockConsumerGroups.consumerGroup1,
         lastActive: timeNow
       }
     };
