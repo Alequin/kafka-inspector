a9984c0b2b1dd5ce75693d4eed8fd741d1d710f3
James Cox <j-cox03@hotmail.com>
2019-03-10 12:39:53 +0000

    Made test for topics resolver

--- src/server/graphql/resolvers/topics-resolver.test.js
+++ src/server/graphql/resolvers/topics-resolver.test.js
@@ -1,13 +1,45 @@
+jest.mock("server-common/kafka/access-global-kafka-connections");
 jest.mock("server-common/kafka/list-topics-with-cache");
+const mockTopics = require("mock-test-data/data/mock-topics");
+const mockListTopics = require("mock-test-data/kafka-node/mock-list-topics");
+const mockAccessGlobalKafkaConnectionsImp = require("mock-test-data/mock-access-global-kafka-connections");
+const accessGlobalKafkaConnections = require("server-common/kafka/access-global-kafka-connections");
+
 const listTopicsWithCache = require("server-common/kafka/list-topics-with-cache");
+
+listTopicsWithCache.mockImplementation(
+  jest.requireActual("server-common/kafka/list-topics-with-cache")
+);
+
+accessGlobalKafkaConnections.mockReturnValue(
+  mockAccessGlobalKafkaConnectionsImp()
+);
+
 const topicsResolver = require("./topics-resolver");
 
 describe("topicsResolver", () => {
   it("makes a request for the cached list of topics and returns the value unmodified", async () => {
-    const mockReturnValue = [{}];
-    listTopicsWithCache.mockResolvedValue(mockReturnValue);
+    const rawTopicObject = mockListTopics.metadata;
+    const expected = [
+      {
+        name: mockTopics.topic1,
+        partitions: [
+          rawTopicObject[mockTopics.topic1][0],
+          rawTopicObject[mockTopics.topic1][1]
+        ]
+      },
+      {
+        name: mockTopics.topic2,
+        partitions: [
+          rawTopicObject[mockTopics.topic2][0],
+          rawTopicObject[mockTopics.topic2][1]
+        ]
+      }
+    ];
+
     const actual = await topicsResolver();
+
     expect(listTopicsWithCache).toBeCalledTimes(1);
-    expect(actual).toBe(mockReturnValue);
+    expect(actual).toEqual(expected);
   });
 });
