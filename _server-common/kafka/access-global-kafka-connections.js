450719b3bf72b968c484d1fcadec7b02ca94e1b7
James Cox <j-cox03@hotmail.com>
2019-03-11 21:30:42 +0000

    Made tests for accessGlobalKafkaConnections

--- _server-common/kafka/access-global-kafka-connections.js
+++ _server-common/kafka/access-global-kafka-connections.js
@@ -1,13 +1,12 @@
 const newClusterConnection = require("./kafka-connections/new-cluster-connection");
 
 const clusters = {};
-const accessGlobalKafkaConnections = kafkaConnectionConfig => {
-  if (!kafkaConnectionConfig || !kafkaConnectionConfig.kafkaBrokers)
+const accessGlobalKafkaConnections = ({ kafkaBrokers } = {}) => {
+  if (!kafkaBrokers)
     throw new Error(
       "No Kafka brokers have been defined in kafkaConnectionConfig. This is required"
     );
 
-  const { kafkaBrokers } = kafkaConnectionConfig;
   if (!clusters[kafkaBrokers]) {
     clusters[kafkaBrokers] = newClusterConnection(kafkaBrokers);
   }
