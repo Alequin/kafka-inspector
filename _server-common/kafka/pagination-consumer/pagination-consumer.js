8db7ebb3628f5d6a63be0227f8a76d1cc7f44fab
James Cox <j-cox03@hotmail.com>
2019-03-16 14:20:43 +0000

    Made tests for pagination consumer

--- _server-common/kafka/pagination-consumer/pagination-consumer.js
+++ _server-common/kafka/pagination-consumer/pagination-consumer.js
@@ -4,9 +4,49 @@ const validMaxOffsets = require("./valid-max-offsets");
 const removeTopicFromConsumer = require("./remove-topic-from-consumer");
 const addNewTopicToConsumer = require("./add-new-topic-to-consumer");
 
-const DEFAULT_OFFSET_RANGE = {
-  min: 0,
-  max: 0
+const removeCurrentPartition = (currentPartition, partitions) => {
+  return partitions.filter(partition => partition !== currentPartition);
+};
+
+const consumeMessage = (
+  consumer,
+  { topicName, minOffset, maxOffsets, partitions },
+  onMessageConsumedCallback
+) => {
+  const processedPartitions = {};
+  const remainingPartitions = removeCurrentPartition(
+    first(consumer.payloads).partition,
+    partitions
+  );
+  return new Promise(resolve => {
+    consumer.on("message", async message => {
+      const currentMaxOffset = maxOffsets[message.partition];
+
+      const isMessageWithinOffsetRange = message.offset <= currentMaxOffset;
+      if (isMessageWithinOffsetRange) {
+        onMessageConsumedCallback(message, message.partition);
+      }
+
+      const shouldProgressToNextPartition =
+        message.offset >= currentMaxOffset &&
+        !processedPartitions[message.partition];
+      if (shouldProgressToNextPartition) {
+        processedPartitions[message.partition] = true;
+        const nextPartition = remainingPartitions.shift();
+        if (nextPartition) {
+          await removeTopicFromConsumer(consumer, topicName);
+          await addNewTopicToConsumer(consumer, {
+            topicName,
+            partition: nextPartition,
+            startingOffset: minOffset
+          });
+        } else {
+          consumer.close();
+          resolve();
+        }
+      }
+    });
+  });
 };
 
 const removeCurrentPartition = (currentPartition, partitions) => {
@@ -57,11 +97,16 @@ const consumeMessage = (
 const validMinOffset = requestedMinOffset => Math.max(requestedMinOffset, 0);
 
 const paginationConsumer = async (
-  { topicName, partitions, offsetRange = DEFAULT_OFFSET_RANGE },
+  {
+    topicName,
+    partitions,
+    requestedMinOffset = 0,
+    requestedMaxOffset = 999999
+  },
   kafkaConnectionConfig,
   onMessageConsumedCallback
 ) => {
-  const minOffset = validMinOffset(offsetRange.min);
+  const minOffset = validMinOffset(requestedMinOffset);
 
   const firstPartitionToConsumeFrom = [
     { topic: topicName, partition: first(partitions), offset: minOffset }
@@ -73,7 +118,7 @@ const paginationConsumer = async (
 
   const maxOffsets = await validMaxOffsets(
     topicName,
-    offsetRange.max,
+    requestedMaxOffset,
     kafkaConnectionConfig
   );
   return consumeMessage(
