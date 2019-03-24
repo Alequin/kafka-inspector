33d708073e5de61b5733ae689fb42330ea53adff
James Cox <j-cox03@hotmail.com>
2019-03-24 23:18:13 +0000

    extended confirmRequestedBrokersAreValid to stop it from erroring if at least one broker is available

--- _server-common/kafka/kafka-connections/confirm-requested-brokers-are-available.js
+++ _server-common/kafka/kafka-connections/confirm-requested-brokers-are-available.js
@@ -4,29 +4,42 @@ const expectedFormatMessage =
   "(make sure the provided format is '<broker-address>:<port>')";
 
 const isBrokerAvailable = broker => {
-  const [brokerHost, port] = broker.split(":");
-  if (!brokerHost || !port)
-    throw new Error(
-      `The given broker was in a bad format: ${broker} / ${expectedFormatMessage}`
-    );
-  tcpp.probe(brokerHost, port, (error, available) => {
-    if (error) {
-      throw new Error(
-        "There was an error while checking if the broker was available: " +
-          error
-      );
+  return new Promise(resolve => {
+    const [brokerHost, port] = broker.split(":");
+    if (!brokerHost || !port) {
+      resolve({
+        error: true,
+        message: `The given broker was in a bad format: ${broker} / ${expectedFormatMessage}`
+      });
     }
-    if (!available) {
-      throw new Error(
-        `broker not available: ${broker} / ${expectedFormatMessage}`
-      );
-    }
+    tcpp.probe(brokerHost, port, (error, available) => {
+      if (error) {
+        resolve({
+          error: true,
+          message:
+            "There was an error while checking if the broker was available: " +
+            error
+        });
+      }
+      if (!available) {
+        resolve({
+          error: true,
+          message: `broker not available: ${broker} / ${expectedFormatMessage}`
+        });
+      }
+      resolve({ error: false, message: "" });
+    });
   });
 };
 
-const confirmRequestedBrokersAreValid = kafkaBrokers => {
-  for (const broker of kafkaBrokers) {
-    isBrokerAvailable(broker);
+const confirmRequestedBrokersAreValid = async kafkaBrokers => {
+  const brokerStatuses = await Promise.all(kafkaBrokers.map(isBrokerAvailable));
+  const areAnyBrokersAvailable = brokerStatuses.some(({ error }) => !error);
+  if (!areAnyBrokersAvailable) {
+    const errorMessages = brokerStatuses
+      .map(({ message }) => message)
+      .join("//");
+    throw new Error(errorMessages);
   }
 };
 
--- _server-common/kafka/kafka-connections/confirm-requested-brokers-are-available.test.js
+++ _server-common/kafka/kafka-connections/confirm-requested-brokers-are-available.test.js
@@ -1,3 +1,4 @@
+const { isError } = require("lodash");
 const mockProbe = jest.fn();
 jest.mock("tcp-ping", () => {
   return {
@@ -31,35 +32,74 @@ describe("confirmRequestedBrokersAreValid", () => {
     expect(secondCall[1]).toBe("9095");
   });
 
-  it("Throws an error if error is true", () => {
+  it("does not error if at least one broker does not error", done => {
+    mockProbe.mockImplementationOnce((_broker, _port, callback) => {
+      const error = true;
+      const available = true;
+      callback(error, available);
+    });
+    mockProbe.mockImplementationOnce((_broker, _port, callback) => {
+      const error = null;
+      const available = true;
+      callback(error, available);
+    });
+
+    confirmRequestedBrokersAreValid(["broker1:9092", "broker2:9092"]).then(
+      done
+    );
+  });
+
+  it("does not error if at least one broker does is available", done => {
+    mockProbe.mockImplementationOnce((_broker, _port, callback) => {
+      const error = false;
+      const available = false;
+      callback(error, available);
+    });
+    mockProbe.mockImplementationOnce((_broker, _port, callback) => {
+      const error = null;
+      const available = true;
+      callback(error, available);
+    });
+
+    confirmRequestedBrokersAreValid(["broker1:9092", "broker2:9092"]).then(
+      done
+    );
+  });
+
+  it("Throws an error if error is true for all brokers", done => {
     mockProbe.mockImplementation((_broker, _port, callback) => {
       const error = true;
       const available = true;
       callback(error, available);
     });
 
-    expect(() =>
-      confirmRequestedBrokersAreValid(["broker1:9092", "broker2:9092"])
-    ).toThrow();
+    confirmRequestedBrokersAreValid(["broker1:9092", "broker2:9092"]).catch(
+      error => {
+        expect(isError(error)).toBe(true);
+        done();
+      }
+    );
   });
 
-  it("Throws an error if broker is not available", () => {
+  it("Throws an error if all brokers are not available", done => {
     mockProbe.mockImplementation((_broker, _port, callback) => {
       const error = null;
       const available = false;
       callback(error, available);
     });
 
-    expect(() =>
-      confirmRequestedBrokersAreValid(["broker1:9092", "broker2:9092"])
-    ).toThrow();
+    confirmRequestedBrokersAreValid(["broker1:9092", "broker2:9092"]).catch(
+      error => {
+        expect(isError(error)).toBe(true);
+        done();
+      }
+    );
   });
 
-  it("Throws an error if a host and a port cannot be identified from any of the given brokers", () => {
-    expect(() => confirmRequestedBrokersAreValid(["broker19092"])).toThrow();
-
-    expect(() =>
-      confirmRequestedBrokersAreValid(["broker1:9092", ""])
-    ).toThrow();
+  it("Throws an error if a host and a port cannot be identified from any of the given brokers", done => {
+    confirmRequestedBrokersAreValid(["broker19092"]).catch(error => {
+      expect(isError(error)).toBe(true);
+      done();
+    });
   });
 });
