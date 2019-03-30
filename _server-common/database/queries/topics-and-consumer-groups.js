f5e0182b56161ee228d9751a47aefe791a0928eb
James Cox <j-cox03@hotmail.com>
2019-03-30 14:22:08 +0000

    Removed unused sql queries

--- _server-common/database/queries/topics-and-consumer-groups.js
+++
@@ -1,7 +0,0 @@
-const runQuery = require("../run-query");
-
-const topicsAndConsumerGroups = async () => {
-  return await runQuery("SELECT * FROM topicsAndConsumerGroups");
-};
-
-module.exports = topicsAndConsumerGroups;
