993c5eb02dc09df43e82ec900b113733a3884d70
James Cox <j-cox03@hotmail.com>
2019-04-04 21:03:37 +0100

    Made unit tests for list-topics

--- _server-common/kafka/list-topics.test.js
+++ _server-common/kafka/list-topics.test.js
@@ -1,55 +1,79 @@
-const mockTopics = require("mock-test-data/data/mock-topics");
-const mockListTopics = require("mock-test-data/kafka-node/mock-list-topics");
+const mockPartitionData = (topicName, partition) => {
+  return {
+    topic: topicName,
+    partition,
+    leader: 1,
+    replicas: [3, 1, 2],
+    isr: [1, 3, 2]
+  };
+};
 
-jest.mock("./access-global-kafka-connections");
-const mockAccessGlobalKafkaConnectionsImp = require("mock-test-data/mock-access-global-kafka-connections");
-const accessGlobalKafkaConnections = require("./access-global-kafka-connections");
+const metadata = {
+  topic1: {
+    "0": mockPartitionData("topic1", 0),
+    "1": mockPartitionData("topic1", 1)
+  },
+  _privateTopic1: {
+    "0": mockPartitionData("_privateTopic1", 0),
+    "1": mockPartitionData("_privateTopic1", 1)
+  },
+  __privateTopic2: {
+    "0": mockPartitionData("__privateTopic2", 0),
+    "1": mockPartitionData("__privateTopic2", 1)
+  }
+};
+
+const listTopicsResponse = [
+  {
+    "1": { nodeId: 1, host: "broker1", port: 9092 },
+    "2": { nodeId: 2, host: "broker2", port: 9092 },
+    "3": { nodeId: 3, host: "broker3", port: 9092 }
+  },
+  { metadata, clusterMetadata: { controllerId: 2 } }
+];
+
+jest.mock("./utils/fetch-broker-details-and-topics-names");
+const fetchBrokerDetailsAndTopicNames = require("./utils/fetch-broker-details-and-topics-names");
+fetchBrokerDetailsAndTopicNames.mockResolvedValue(listTopicsResponse);
 
 const listTopics = require("./list-topics");
 
-describe.skip("topics", () => {
-  it("Should return a list of all topics, filtering out private topics", async () => {
-    accessGlobalKafkaConnections.mockReturnValue(
-      mockAccessGlobalKafkaConnectionsImp()
-    );
-
-    const topics = await listTopics();
-    const rawTopicObject = mockListTopics.metadata;
-    expect(topics).toEqual([
-      {
-        name: mockTopics.topic1,
-        partitions: [
-          rawTopicObject[mockTopics.topic1][0],
-          rawTopicObject[mockTopics.topic1][1]
-        ]
-      },
-      {
-        name: mockTopics.topic2,
-        partitions: [
-          rawTopicObject[mockTopics.topic2][0],
-          rawTopicObject[mockTopics.topic2][1]
-        ]
-      }
-    ]);
+describe("topics", () => {
+  beforeEach(() => {
+    fetchBrokerDetailsAndTopicNames.mockClear();
   });
 
-  it("Should throw an error if requesting the list of topics fails", done => {
-    const mockErrorMessage = "list topics error message";
-    accessGlobalKafkaConnections.mockReturnValue(
-      mockAccessGlobalKafkaConnectionsImp([
-        {
-          path: "kafkaNode.admin.listTopics",
-          override: callback => {
-            const error = mockErrorMessage;
-            callback(error, null);
-          }
-        }
-      ])
-    );
+  it("Calls fetchBrokerDetailsAndTopicNames with the given kafka config", async () => {
+    const kafkaConfig = { kafkaBrokers: ["broker1:9092"] };
+    await listTopics(kafkaConfig);
+    expect(fetchBrokerDetailsAndTopicNames).toHaveBeenCalledTimes(1);
+    expect(fetchBrokerDetailsAndTopicNames).toHaveBeenCalledWith(kafkaConfig);
+  });
 
-    listTopics().catch(error => {
-      expect(error).toBe(mockErrorMessage);
-      done();
-    });
+  it("Returns a list of all topics, filtering out private topics", async () => {
+    const expected = [
+      {
+        name: "topic1",
+        partitions: [
+          {
+            topic: "topic1",
+            partition: 0,
+            leader: 1,
+            replicas: [3, 1, 2],
+            isr: [1, 3, 2]
+          },
+          {
+            topic: "topic1",
+            partition: 1,
+            leader: 1,
+            replicas: [3, 1, 2],
+            isr: [1, 3, 2]
+          }
+        ]
+      }
+    ];
+
+    const actual = await listTopics();
+    expect(actual).toEqual(expected);
   });
 });
