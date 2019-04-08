d01aa29a63349cbc68d594f47b9cf810c3bc942c
James Cox <j-cox03@hotmail.com>
2019-04-08 20:42:41 +0100

    Changed environment to always set the babel-env and node-env

--- _server-common/config/environment.js
+++ _server-common/config/environment.js
@@ -4,21 +4,20 @@ const isTest = process.env.NODE_ENV === TEST;
 const isProduction = process.env.NODE_ENV === PRODUCTION;
 // Default to development if nothing is set
 const isDevelopment = !isProduction && !isTest;
-if (isDevelopment) {
-  process.env.BABEL_ENV = DEVELOPMENT;
-  process.env.NODE_ENV = DEVELOPMENT;
-}
 
-const { freeze } = Object;
 const environmentVariables = {
   isProduction,
   isDevelopment,
   isTest
 };
 
-module.exports = freeze({
+const currentEnvironment = identifyCurrentEnvironment(environmentVariables);
+process.env.BABEL_ENV = currentEnvironment;
+process.env.NODE_ENV = currentEnvironment;
+
+module.exports = Object.freeze({
   ...environmentVariables,
-  currentEnvironment: identifyCurrentEnvironment(environmentVariables)
+  currentEnvironment
 });
 
 function identifyCurrentEnvironment({ isProduction, isDevelopment, isTest }) {
